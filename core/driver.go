8704f87cb48ab254e8f21dbcd8eec274ed8fba01
Shintaro Kaneko <kaneshin0120@gmail.com>
2016-06-19 01:54:01 +0000

    Add driver to migrate files

---
+++ core/driver.go
@@ -0,0 +1,51 @@
+package core
+
+import (
+	"sync"
+)
+
+type (
+	// Driver interface.
+	Driver interface {
+		// Open is the first function to be called.
+		// Check the dsn string and open and verify any connection
+		// that has to be made.
+		Open(string) error
+
+		// Close is the last function to be called.
+		// Close any open connection here.
+		Close() error
+
+		// Version returns the current migration version.
+		Version() (uint64, error)
+	}
+)
+
+var (
+	registry = struct {
+		mu      sync.RWMutex
+		drivers map[string]Driver
+	}{drivers: make(map[string]Driver)}
+)
+
+// RegisterDriver a driver so it can be created from its name. Drivers should
+// call this from an init() function so that they registers themselvse on
+// import
+func RegisterDriver(name string, d Driver) {
+	if d == nil {
+		panic("driver: register driver is nil")
+	}
+	registry.mu.Lock()
+	defer registry.mu.Unlock()
+	if _, dup := registry.drivers[name]; dup {
+		panic("sql: register called twice for driver " + name)
+	}
+	registry.drivers[name] = d
+}
+
+// GetDriver retrieves a registered driver by name.
+func GetDriver(name string) Driver {
+	registry.mu.RLock()
+	defer registry.mu.RUnlock()
+	return registry.drivers[name]
+}
---
+++ core/driver_test.go
@@ -0,0 +1,33 @@
+package core
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type drv struct {
+}
+
+func (d *drv) Open(_ string) error      { return nil }
+func (d *drv) Close() error             { return nil }
+func (d *drv) Version() (uint64, error) { return 0, nil }
+
+func TestRegisterDriver(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.NotNil(registry)
+
+	assert.Panics(func() {
+		RegisterDriver("nil", (Driver)(nil))
+	})
+
+	// gets and sets
+	assert.Nil(GetDriver("driver"), "should be nil.")
+	RegisterDriver("driver", &drv{})
+	assert.NotNil(GetDriver("driver"), "should be retrieve a registered driver.")
+
+	assert.Panics(func() {
+		RegisterDriver("driver", &drv{})
+	}, "should be panic if registering driver twice.")
+}

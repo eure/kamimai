ab4add5fb0206523e9ddca34f23c84859ec59c63
Shintaro Kaneko <kaneshin0120@gmail.com>
2016-09-15 10:17:23 +0000

    Add driver test

--- kamimai_test.go
+++ kamimai_test.go
@@ -53,3 +53,49 @@ func TestSync(t *testing.T) {
 		}
 	}
 }
+
+func TestUp(t *testing.T) {
+	assert := assert.New(t)
+
+	conf, err := core.NewConfig("examples/testdata")
+	if assert.NoError(err) {
+		conf.WithEnv("development")
+
+		defer func() {
+			// Down
+			assert.NoError(Down(conf))
+			ver, err := Current(conf)
+			if assert.NoError(err) {
+				assert.EqualValues(0, ver)
+			}
+		}()
+
+		// Down
+		assert.NoError(Down(conf))
+		ver, err := Current(conf)
+		if assert.NoError(err) {
+			assert.EqualValues(0, ver)
+		}
+
+		// Up
+		assert.NoError(Up(conf))
+		ver, err = Current(conf)
+		if assert.NoError(err) {
+			assert.EqualValues(1, ver)
+		}
+
+		// Down
+		assert.NoError(Down(conf))
+		ver, err = Current(conf)
+		if assert.NoError(err) {
+			assert.EqualValues(0, ver)
+		}
+
+		// Up
+		assert.NoError(Up(conf))
+		ver, err = Current(conf)
+		if assert.NoError(err) {
+			assert.EqualValues(1, ver)
+		}
+	}
+}

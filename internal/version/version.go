2ba95a25374d580096252cfd45747ea1e53652c9
Shintaro Kaneko <kaneshin0120@gmail.com>
2020-01-01 21:21:04 +0900

    Add IsTimestamp function to check number is kind of timestamp or not

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,6 +3,8 @@ package version
 import (
 	"fmt"
 	"regexp"
+	"strconv"
+	"time"
 )
 
 var (
@@ -11,7 +13,6 @@ var (
 
 // Get returns a string which contains numbers.
 func Get(name string) string {
-
 	for i := 0; i < len(name); i++ {
 		if name[i] == '_' {
 			str := name[:i]
@@ -27,7 +28,6 @@ func Get(name string) string {
 
 // Format returns a version format for printing.
 func Format(name string) string {
-
 	ver := Get(name)
 	if len(ver) == 0 {
 		return ""
@@ -38,4 +38,16 @@ func Format(name string) string {
 	}
 
 	return fmt.Sprintf("%%0%dd", len(ver))
+}
+
+// IsTimestamp returns if value is timestamp or not
+func IsTimestamp(value uint64) bool {
+	const layout = "20060102150405"
+	str := strconv.FormatUint(value, 10)
+	t, err := time.Parse(layout, str)
+	if err != nil {
+		return false
+	}
+	// compare by zero-unixtime
+	return t.After(time.Unix(0, 0))
 }
